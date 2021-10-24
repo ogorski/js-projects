3a5da03a5909e8375efb533b3518cc9be4445d4c
Olegio <ogorski23@gmail.com>
2021-10-24 21:43:40 +0300

    update
    added basics

---
+++ basics/basic02/index.js
@@ -0,0 +1,23 @@
+let car;
+car = 'Volkswagen';
+console.log(car);
+
+let math_plus;
+math_plus = 2+2;
+console.log(math_plus);
+
+let cars;
+cars = ['Volkswagen', 'BMW', 'Lamborgini'];
+console.log(cars);
+
+let name = 'Oleh';
+console.log(name);
+
+// Variables cannot be as reserved keyword
+// Variables should and must be meaningful
+// Variables cannot start with number
+// Variables cannot contain spaces or/and hyphen (-)
+
+let firstName = 'Oleh'; 
+let lastName = 'Horskyj';
+console.log(firstName, lastName);
